4367fe171acbd056fee25a4cb5e5d45388d8fd44
kyuiazbek <kyuiazbekchoibekov@gmail.com>
2025-07-08 13:04:44 +0600

    Update ReitingDetail.jsx

--- src/ui/ReitingDetail/ReitingDetail.jsx
+++ src/ui/ReitingDetail/ReitingDetail.jsx
@@ -8,12 +8,12 @@ const CircleReiting = ({ number }) => {
         value={number}
         maxValue={10}
         text={number}
-        styles={buildStyles({
+{/*         styles={buildStyles({
           // pathColor: "white",
           trailColor: "rgba(255,255,255,0.2)",
           textColor: "white",
           pathColor: number < 5 ? "red" : number < 7 ? "orange" : "green",
-        })}
+        })} */}
       />
     </div>
   );
